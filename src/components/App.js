506d9e481c422f5df8e32ae3908928646c30f6ff
Sadia Naaz <sadianaaz110@gmail.com>
2024-03-03 20:30:15 +0530

    submitting code-Sun Mar 03 2024 20:30:13 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -5,16 +5,17 @@ const App = () => {
   return (
     <>
     <div>
-        <Tooltip text={"This is s tooltip"}>
-        <p>Hover Over Me</p>
+        <Tooltip text={"This is a tooltip"}>
+      
+        <h2>Hover Over Me</h2> {/* children elements */}
         </Tooltip>
     </div>
-     <div>
-     <Tooltip text={"This is s tooltip"}>
-     <p>Hover Over Me to see another tooltip</p>
+    <div>
+     <Tooltip text={"This is a tooltip"}>
+     <p>Hover Over Me to see another tooltip</p> {/* children elements */}
      </Tooltip>
- </div>
- </>
+     </div>
+     </>
   )
 }
 
--- src/tooltip.js
+++ src/tooltip.js
@@ -5,18 +5,19 @@ import React, { useState } from "react";
  export const Tooltip = ({text,children})=>{
 
   const [showtooltip,setshowTooltip]=useState(false);
-const show=()=>{
+ const show=()=>{
   setshowTooltip(true);
 }
-const hide=()=>{
+ const hide=()=>{
   setshowTooltip(false);
 }
 
 
 return<>
-{showtooltip && <div className="tooltip">
+ 
+{showtooltip && 
   <b className="tooltiptext">{text}</b>
-  </div>}
+  }
   <div onMouseEnter={show} onMouseLeave={hide}>
   {children}
   </div>
